5cdf28850bfe6558c504537c38275ca9a7577917
Carlo Cadiz <carlocadiz71@gmail.com>
2019-07-23 16:09:21 -0400

    array examples

--- script.js
+++ script.js
@@ -22,4 +22,38 @@ var checkDriverAge2 = function () {
   }
 }
 
-checkDriverAge(21);
+//checkDriverAge(21);
+
+
+// using this array,
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+// 1. Remove the Banana from the array.
+
+array.shift();
+console.log(array);
+
+// 2. Sort the array in order.
+console.log(array.sort());
+
+// 3. Put "Kiwi" at the end of the array.
+array.push("Kiwi");
+console.log(array);
+
+// 4. Remove "Apples" from the array.
+array.shift();
+console.log(array);
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+console.log(array.reverse());
+// you should have at the end:
+// ["Kiwi", "Oranges", "Blueberries"]
+
+// using this array,
+ var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+console.log(array2[1][1][0]);
+
+
